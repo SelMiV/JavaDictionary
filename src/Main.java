1a8a93c2f7c328ac75372dff316a9a81d89f8524
SelMiV <selivankin2005@gmail.com>
2026-05-08 02:27:09 +0300

    добавил возможность смены словаря, выхода из программы и автоматическое создание файла, если такого не существует

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,143 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите название файла для сохранения словаря:");
-        String fileName = scanner.next();
+        while (true) {
 
+            System.out.print("Введите название файла для сохранения словаря: ");
+            String fileName = scanner.next();
 
-        System.out.println("Тип словаря( 1 или 2)");
-        DictionaryManager dictionaryManager = null;
-        switch (scanner.nextInt()){
-            case 1:
-                dictionaryManager = new DictionaryManagerForFistLg( fileName);
-                break;
-            case 2:
-                dictionaryManager = new DictionaryManagerForSecondLg(fileName);
-                break;
-            default:
-                System.out.println("Такого словаря нет. Выберите повторно");
-                break;
-        }
-        if (dictionaryManager != null) {
-            while (true) {
-                System.out.println(
-                        "1) Просмотр содержимого словаря\n" +
-                                "2) Просмотр элемента по ключу\n" +
-                                "3) Добавление элемента\n" +
-                                "4) Удаление элемента\n"
-                );
-                switch (scanner.nextInt()) {
-                    case 1: {
-                        System.out.println("1) Содержимое:");
-                        System.out.println(dictionaryManager.getAllWords());
-                        break;
-                    }
-                    case 2: {
-                        System.out.println("2) Элемент:");
-                        try{
-                            System.out.println(dictionaryManager.findWord(scanner.next()));
-                        }
-                        catch(Exception e){
-                            System.out.println(e.getMessage());
-                        }
-                        break;
-                    }
-                    case 3: {
-                        System.out.println("3) Введите слово:");
-                        String key = scanner.next();
-                        System.out.println("Введите перевод:");
-                        String value = scanner.next();
-                        try{
-                            dictionaryManager.addWord(key, value);
-                            System.out.println("Успешно!")  ;
-                        } catch (Exception e) {
-                            System.out.println(e.getMessage());
-                        }
-                        break;
-                    }
-                    case 4: {
-                        System.out.println("4) Введите слово:");
-                        try{
-                            dictionaryManager.deleteWord(scanner.next());
-                            System.out.println("Успешно!");
-                        }
-                        catch (Exception e){
-                            System.out.println(e.getMessage());
-                        }
-                        break;
-                    }
-                    default:{
-                        System.out.println("Неверная команда.");
-                    }
+            File file = new File(fileName);
+
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                    System.out.println("Файл создан.");
+                } catch (IOException e) {
+                    System.out.println("Ошибка при создании файла: " + e.getMessage());
+                    return;
                 }
-                System.out.println();
+            }
+
+            System.out.println("Тип словаря( 1 или 2)");
+            DictionaryManager dictionaryManager = null;
+            switch (scanner.nextInt()) {
+                case 1:
+                    dictionaryManager = new DictionaryManagerForFistLg(fileName);
+                    break;
+                case 2:
+                    dictionaryManager = new DictionaryManagerForSecondLg(fileName);
+                    break;
+                default:
+                    System.out.println("Такого словаря нет. Выберите повторно");
+                    continue;
+            }
+
+            if (dictionaryManager != null) {
+
+                dictionaryLoop:
+                while (true) {
+
+                    System.out.println(
+                            "1) Просмотр содержимого словаря\n" +
+                                    "2) Просмотр элемента по ключу\n" +
+                                    "3) Добавление элемента\n" +
+                                    "4) Удаление элемента\n" +
+                                    "5) Сменить словарь\n" +
+                                    "6) Выход\n"
+                    );
+
+                    switch (scanner.nextInt()) {
+
+                        case 1: {
+
+                            System.out.println("1) Содержимое:");
+
+                            String words = dictionaryManager.getAllWords();
+
+                            if (words == null || words.trim().isEmpty()) {
+                                System.out.println("Словарь пуст.");
+                            } else {
+                                System.out.println(words);
+                            }
+
+                            break;
+                        }
+
+                        case 2: {
+
+                            System.out.println("2) Элемент:");
+
+                            try {
+                                System.out.println(
+                                        dictionaryManager.findWord(scanner.next())
+                                );
+                            }
+                            catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+
+                            break;
+                        }
+
+                        case 3: {
+
+                            System.out.println("3) Введите слово:");
+                            String key = scanner.next();
+
+                            System.out.println("Введите перевод:");
+                            String value = scanner.next();
+
+                            try {
+                                dictionaryManager.addWord(key, value);
+                                System.out.println("Успешно!");
+                            }
+                            catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+
+                            break;
+                        }
+
+                        case 4: {
+
+                            System.out.println("4) Введите слово:");
+
+                            try {
+                                dictionaryManager.deleteWord(scanner.next());
+                                System.out.println("Успешно!");
+                            }
+                            catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+
+                            break;
+                        }
+
+                        case 5: {
+
+                            System.out.println("Переход к выбору словаря...");
+                            break dictionaryLoop;
+                        }
+
+                        case 6: {
+
+                            System.out.println("Выход...");
+                            return;
+                        }
+
+                        default: {
+                            System.out.println("Неверная команда.");
+                        }
+                    }
+
+                    System.out.println();
+                }
             }
         }
     }
